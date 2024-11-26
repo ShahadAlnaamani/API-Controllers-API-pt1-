e9e1329be7d766f8510a36efaaaf2bea9213ccf6
Shahad <Shahadalnaamani@outlook.com>
2024-11-26 13:38:27 +0400

    first commit

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using ServicesLab1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicesLab1
+{
+    public class ApplicationDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(" Data Source=(local); Initial Catalog=BankTest; Integrated Security=true; TrustServerCertificate=True ");
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<BankAccount> BankAccounts { get; set; }    
+    }
+}
